45b0d40c3cdd5f053f4b0f2d197a31538152b513
Sarapdx <saraswathi.rajkumar1@gmail.com>
2024-05-25 22:17:02 -0700

    setup
    added setup and teardown

--- DataDrivenFramework/src/test/java/com/ddf/base/TestBase.java
+++ DataDrivenFramework/src/test/java/com/ddf/base/TestBase.java
@@ -1,10 +1,35 @@
 package com.ddf.base;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
 public class TestBase {
+	
+	/*
+	 * initialize WebDriver
+	 * initialize Properties
+	 * initialize Logs
+	 * initialize ExtendReports
+	 * initialize DB
+	 * initialize Excel reader
+	 * initialize Mailing
+	 * 
+	 */
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp()
+	{
+		
+	}
+	
+	@AfterSuite
+	public void tearDown()
+	{
+		
 	}
 
 }
