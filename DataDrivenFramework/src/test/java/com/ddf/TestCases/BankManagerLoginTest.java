8627ced04d610ae2bce0b4a1f9a6398e70790ee6
Sarapdx <saraswathi.rajkumar1@gmail.com>
2024-05-28 20:57:21 -0700

    add html report
    Html report added through listeners

--- DataDrivenFramework/src/test/java/com/ddf/TestCases/BankManagerLoginTest.java
+++ DataDrivenFramework/src/test/java/com/ddf/TestCases/BankManagerLoginTest.java
@@ -2,6 +2,7 @@ package com.ddf.TestCases;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import com.ddf.base.TestBase;
@@ -20,7 +21,8 @@ public class BankManagerLoginTest extends TestBase{
 		//Assert.assertTrue(isElementPresent(By.cssSelector(objrepo.getProperty("addCustBtn_CSS"))),"Navigated to next page");
 		
 		log.debug("Login successfully executed");
-		
+		Reporter.log("Login successfully executed");
+		Reporter.log("<a href=\"C:\\Users\\kfamily\\eclipse-workspace\\DataDrivenFramework\\src\\test\\resources\\excel\\download.png\">Screenshot</a>");
 	}
-
 }
+		
